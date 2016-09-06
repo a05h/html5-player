bb9b4bcd8a80d76e317d3736cdd85ed737dfdd3f
a05h <a117q35mt@gmail.com>
2016-09-06 00:11:44 -0400

    progressbar and tags fix

--- source/javascripts/events.js
+++ source/javascripts/events.js
@@ -1,5 +1,4 @@
-
-window.addEventListener('load', function() {
+/*window.addEventListener('load', function() {
   videoArea = document.getElementById('video-element');
   videoArea.addEventListener('click', play, false);
   buttonPlay = document.getElementById('button-play');
@@ -41,7 +40,7 @@ function progressVideo() {
   }
 };
 
-/*function setProgress(clickArea) {
+function setProgress(clickArea) {
   var width = window.getComputedStyle(progressbarDone).getPropertyValue('width');
   width = parseFloat(width.substr(0, width.length - 1));
   videoArea.currentTime = ((clickArea.pageX - progressbarDone.offsetLeft) / width) * videoArea.duration;
@@ -49,3 +48,58 @@ function progressVideo() {
   window.clearInterval(checkProgress);
   progressVideo();
 };*/
+
+
+
+window.addEventListener('load', function() {
+  video = document.getElementById('video');
+  video.addEventListener('click', eventPlay, false);
+  buttonPlay = document.getElementById('button-play');
+  buttonPlay.addEventListener('click', eventPlay, false);
+  muteButton = document.getElementById('button-mute');
+  muteButton.addEventListener('click', eventMute, false);
+  buttonFullscreen = document.getElementById('button-fullscreen');
+  buttonFullscreen.addEventListener('click', eventFullscreen, false);
+
+  progressbarContainer = document.getElementById('progressbar-container');
+  progressbarContainer.addEventListener('click', setProgress, false);
+  volumeContainer = document.getElementById('volume-container');
+  volumeContainer.addEventListener('click', setVolume, false);
+  progressbar = document.getElementById('progressbar');
+  volume = document.getElementById('volume');
+  timeDone = document.getElementById('time-done');
+}, false);
+
+function eventPlay() {
+  var checkProgress = setInterval(progressVideo, 0);
+  if (video.paused) {
+    video.play();
+    buttonPlay.src = './source/images/button-volume.png';
+  } else {
+    video.pause();
+    buttonPlay.src = './source/images/button-play.png';
+    window.clearInterval(checkProgress);
+  }
+};
+
+function eventMute() {
+};
+
+function eventFullscreen() {
+};
+
+function progressVideo() {
+  var result = (video.currentTime / video.duration) * 100;
+  progressbar.style.width = result + '%';
+  if (video.ended || video.paused) {
+    buttonPlay.src = './source/images/button-play.png';
+  }
+};
+
+function setProgress(clickArea) {
+  var width = window.getComputedStyle(progressbarContainer).getPropertyValue('width');
+  width = parseFloat(width.substr(0, width.length - 1));
+  video.currentTime = ((clickArea.pageX - progressbarContainer.offsetLeft) / width) * video.duration;
+  window.clearInterval(checkProgress);
+  progressVideo();
+};
